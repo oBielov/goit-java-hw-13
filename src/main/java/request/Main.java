f9b8cc905d26e7e49ebf85e6e0edb053ebbbe677
oBielov <i.byelov@gmail.com>
2021-05-29 01:33:28 +0300

    update

--- src/main/java/request/Main.java
+++ src/main/java/request/Main.java
@@ -13,31 +13,4 @@ public class Main {
         List<User> result = execute(client.getUsers());
         for (User user : result) System.out.println(user.getCompany().getName());
     }
-
-//    public static User newUser() {
-//        return new User.UserBuilder()
-//                .id(13)
-//                .name("Roman Petrenko")
-//                .userName("Romchansky")
-//                .email("petrenko.roman@hotmail.com")
-//                .address(new Address.AddressBuilder()
-//                        .street("St Claude Ave")
-//                        .apartment("Apt. 4808")
-//                        .city("New Orleans")
-//                        .zipcode("704545")
-//                        .geo(new Geo.GeoBuilder()
-//                                .latitude(29.58)
-//                                .longitude(90.06)
-//                                .build())
-//                        .build())
-//                .phoneNumber("+38066666666")
-//                .website("https://github.com/Romchansky")
-//                .company(new Company.CompanyBuilder()
-//                        .name("Sun microsystems")
-//                        .catchPhrase("Hello world")
-//                        .bs("Java").build())
-//                .build();
-//
-//
-//    }
 }
